e720d62c057f7cb3f5b81e85e55d04540df536b3
Cyross Makoto <cyross@po.twin.ne.jp>
2008-12-13 00:45:35 +0900

    refactor Slide module

--- lib/Miyako/EXT/slides.rb
+++ lib/Miyako/EXT/slides.rb
@@ -38,18 +38,38 @@ module Miyako
       tmp[:size]  ||= Size.new(640, 480)
       tmp[:type]  ||= :ac
       tmp[:color] ||= Color[:white]
-      return Parts.new(Sprite.new(tmp).fill(tmp[:color])).main_bottom
+      return Parts.new(tmp[:size]).tap{|obj| obj[:___base___] = Sprite.new(tmp).fill(tmp[:color])}
     end
     
-    @@templates["320x240"] = {:size=>Size.new(320,240)}
-    @@templates["640x480"] = {}
-    @@templates["800x600"] = {:size=>Size.new(800,600)}
-    @@templates["gray"]    = {:color=>[128,128,128,255]}
+    @@templates["320x240"]    = {:size=>Size.new(320,240)}
+    @@templates["640x480"]    = {}
+    @@templates["800x600"]    = {:size=>Size.new(800,600)}
+    @@templates["white"]      = {:color=>[255,255,255,255]}
+    @@templates["gray"]       = {:color=>[128,128,128,255]}
+    @@templates["black"]      = {:color=>[0,0,0,255]}
+    @@templates["half-white"] = {:color=>[255,255,255,128]}
 
+    #===スライドをテンプレート文字列から作成する
+    #スライドを所定の名称で生成する。利用できるのは以下の7種類
+    #"320x240"    大きさは320x240ピクセル、背景は白色
+    #"640x480"    大きさは640x480ピクセル、背景は白色(デフォルトのテンプレート)
+    #"800x600"    大きさは800x600ピクセル、背景は白色
+    #"white"      "640x480"と同一
+    #"gray"       大きさは640x480ピクセル、背景は灰色([128,128,128,255])
+    #"black"      大きさは640x480ピクセル、背景は黒色([0,0,0,255])
+    #"half-white" 背景が半分透明な"white"([255,255,255,128])
+    #_sym_:: テンプレートに対応した文字列
+    #返却値:: 生成したスライド(Partsクラスインスタンス)
     def Slide.[](sym = "640x480")
       return Slide.create(@@templates[sym])
     end
     
+    #===スライドのテンプレートを追加する
+    #指定できるテンプレートの内容は、Sprite.newメソッドの引数がそのまま使える(Hashクラスインスタンスとして渡す)
+    #また、追加として、:colorパラメータを使って塗りつぶす色を指定することが出来る。
+    #(例):color=>Color[:red]
+    #_sym_:: テンプレート名
+    #_params_:: 生成時のパラメータ
     def Slide.[]=(sym, params)
       @@templates[sym] = params
     end
@@ -85,7 +105,25 @@ module Miyako
     #返却値:: 自分自身を返す
     def []=(title, objs)
       @body[title] = objs
-      @body.main_bottom
+      return self
+    end
+
+    #===スライドを画面に描画する
+    #ブロックを渡すと、スライド,画面側のSpriteUnitを更新して、それを実際の転送に反映させることが出来る。
+    #ブロックの引数は、|スライド側SpriteUnit,画面側SpriteUnit|となる。
+    #返却値:: 自分自身を返す
+    def render(&block)
+      @body.render(&block)
+      return self
+    end
+
+    #===スライドを画像に描画する
+    #ブロックを渡すと、スライド,画像側のSpriteUnitを更新して、それを実際の転送に反映させることが出来る。
+    #ブロックの引数は、|スライド側SpriteUnit,画像側SpriteUnit|となる。
+    #_dst_:: 描画先画像(Spriteクラスインスタンスなど)
+    #返却値:: 自分自身を返す
+    def render_to(dst, &block)
+      @body.render(dst, &block)
       return self
     end
 
@@ -93,11 +131,6 @@ module Miyako
     def dispose
     end
     
-    def_delegators(:@body,
-                   :main_top, :main_bottom,
-                   :max_dp, :min_dp, :sort_dp,
-                   :remove, :each,
-                   :show, :hide, :start, :stop, :reset,
-                   :viewport, :viewport=)
+    def_delegators(:@body, :remove, :each, :start, :stop, :reset, :update, :update_animation)
   end
 end
