7bb564e642bc0f56a7b746081581ef2272adaa60
Cyross Makoto <cyross@po.twin.ne.jp>
2008-12-14 23:18:07 +0900

    collect Drawing interface

--- lib/Miyako/API/drawing.rb
+++ lib/Miyako/API/drawing.rb
@@ -41,70 +41,69 @@ module Miyako
                                  :fill   => {:solid         => lambda{|b, l| b.draw_filled_ellipse(*l)},
                                              :anti_aliasing => lambda{|b, l| b.drawAAFilledEllipse(*l)}}}}
 
-	#===画像全体を指定の色で塗りつぶす
-    #_bitmap_:: 描画対象のビットマップ(Sprite.bitmapやSpriteUnit#bitmapの返り値など)
+    #===画像全体を指定の色で塗りつぶす
+    #_sprite_:: 描画対象のスプライト(SDL::Surfaceクラスインスタンスを返すbitmapメソッドを持つインスタンス)
     #_color_:: 塗りつぶす色。Color.to_rgbメソッドのパラメータでの指定が可能
     #返却値:: 自分自身を返す
-    def Drawing.fill(bitmap, color)
-      bitmap.fill_rect(0,0,self.w,self.h, Color.to_rgb(color))
+    def Drawing.fill(sprite, color)
+      sprite.bitmap.fill_rect(0,0,sprite.bitmap.w,sprite.bitmap.h, Color.to_rgb(color))
       return self
     end
 
     #===画像内に直線を引く
-    #_bitmap_:: 描画対象のビットマップ(Sprite.bitmapやSpriteUnit#bitmapの返り値など)
+    #_sprite_:: 描画対象のスプライト(SDL::Surfaceクラスインスタンスを返すbitmapメソッドを持つインスタンス)
     #_rect_:: 描画する矩形。画像の左上を[0,0]とする。4要素の整数の配列かRect構造体を使用
     #_color_:: 描画色。Color.to_rgbメソッドのパラメータでの指定が可能
     #_attribute_:: 描画の属性。:normal固定。
     #_aa_:: アンチエイリアスの指定。:solidでオフ、:anti_aliasingでオン
     #返却値:: 自分自身を返す
-    def Drawing.line(bitmap, rect, color, attribute = :normal, aa = :solid)
+    def Drawing.line(sprite, rect, color, attribute = :normal, aa = :solid)
       raise MiyakoError, "not have Drawing attribute! #{attribute}" unless @@draw_list[:line].has_key?(attribute)
-      raise MiyakoError, "not have Drawing aaa! #{aa}" unless @@draw_list[:line][:attribute].has_key?(aa)
-      color = 
-      @@draw_list[:line][attribute][aa].call(bitmap, rect.to_a << Color.to_rgb(color))
+      raise MiyakoError, "not have Drawing anti aliasing mode! #{aa}" unless @@draw_list[:line][attribute].has_key?(aa)
+      @@draw_list[:line][attribute][aa].call(sprite.bitmap, rect.to_a << Color.to_rgb(color))
       return self
     end
 
     #===画像内に矩形を描画する
-    #_bitmap_:: 描画対象のビットマップ(Sprite.bitmapやSpriteUnit#bitmapの返り値など)
+    #_sprite_:: 描画対象のスプライト(SDL::Surfaceクラスインスタンスを返すbitmapメソッドを持つインスタンス)
     #_rect_:: 描画する矩形。画像の左上を[0,0]とする。4要素の整数の配列かRect構造体を使用
     #_color_:: 描画色。Color.to_rgbメソッドのパラメータでの指定が可能
     #_attribute_:: 描画の属性。:normalで縁のみ描画、:fillで内部も塗りつぶす
     #_aa_:: アンチエイリアスの指定。:solidでオフ、:anti_aliasingでオン
     #返却値:: 自分自身を返す
-    def Drawing.rect(bitmap, rect, color, attribute = :normal, aa = :solid)
-      raise MiyakoError, "not have Drawing attribute! #{attribute}" unless @@draw_list[:line].has_key?(attribute)
-      raise MiyakoError, "not have Drawing aaa! #{aa}" unless @@draw_list[:line][:attribute].has_key?(aa)
-      @@draw_list[:rect][attribute][aa].call(bitmap, rect.to_a << Color.to_rgb(color))
+    def Drawing.rect(sprite, rect, color, attribute = :normal, aa = :solid)
+      raise MiyakoError, "not have Drawing attribute! #{attribute}" unless @@draw_list[:rect].has_key?(attribute)
+      raise MiyakoError, "not have Drawing anti aliasing mode! #{aa}" unless @@draw_list[:rect][attribute].has_key?(aa)
+      @@draw_list[:rect][attribute][aa].call(sprite.bitmap, rect.to_a << Color.to_rgb(color))
       return self
     end
 
     #===画像内に円を描画する
-    #_bitmap_:: 描画対象のビットマップ(Sprite.bitmapやSpriteUnit#bitmapの返り値など)
+    #_sprite_:: 描画対象のスプライト(SDL::Surfaceクラスインスタンスを返すbitmapメソッドを持つインスタンス)
     #_point_:: 中心の位置。2要素の整数の配列、もしくはPoint構造体を使用可能
     #_r_:: 円の半径。整数を使用可能。
     #_color_:: 描画色。Color.to_rgbメソッドのパラメータでの指定が可能
     #_attribute_:: 描画の属性。:normalで縁のみ描画、:fillで内部も塗りつぶす
     #_aa_:: アンチエイリアスの指定。:solidでオフ、:anti_aliasingでオン
     #返却値:: 自分自身を返す
-    def Drawing.circle(bitmap, point, r, color, attribute = :normal, aa = :solid)
-      raise MiyakoError, "not have Drawing attribute! #{attribute}" unless @@draw_list[:line].has_key?(attribute)
-      raise MiyakoError, "not have Drawing aaa! #{aa}" unless @@draw_list[:line][:attribute].has_key?(aa)
-      @@draw_list[:circle][attribute][aa].call(bitmap, point.to_a << r << Color.to_rgb(color))
+    def Drawing.circle(sprite, point, r, color, attribute = :normal, aa = :solid)
+      raise MiyakoError, "not have Drawing attribute! #{attribute}" unless @@draw_list[:circle].has_key?(attribute)
+      raise MiyakoError, "not have Drawing anti aliasing mode! #{aa}" unless @@draw_list[:circle][attribute].has_key?(aa)
+      @@draw_list[:circle][attribute][aa].call(sprite.bitmap, point.to_a << r << Color.to_rgb(color))
       return self
     end
 
     #===画像内に楕円を描画する
-    #_bitmap_:: 描画対象のビットマップ(Sprite.bitmapやSpriteUnit#bitmapの返り値など)
+    #_sprite_:: 描画対象のスプライト(SDL::Surfaceクラスインスタンスを返すbitmapメソッドを持つインスタンス)
     #_rect_:: 描画する矩形。画像の左上を[0,0]とする。4要素の整数の配列かRect構造体を使用
     #_color_:: 描画色。Color.to_rgbメソッドのパラメータでの指定が可能
     #_attribute_:: 描画の属性。:normalで縁のみ描画、:fillで内部も塗りつぶす
     #_aa_:: アンチエイリアスの指定。:solidでオフ、:anti_aliasingでオン
     #返却値:: 自分自身を返す
-    def Drawing.ellipse(bitmap, rect, color, attribute = :normal, aa = :solid)
-      raise MiyakoError, "not have Drawing attribute! #{attribute}" unless @@draw_list[:line].has_key?(attribute)
-      raise MiyakoError, "not have Drawing aaa! #{aa}" unless @@draw_list[:line][:attribute].has_key?(aa)
-      @@draw_list[:ellipse][attribute][aa].call(bitmap, rect.to_a << Color.to_rgb(color))
+    def Drawing.ellipse(sprite, rect, color, attribute = :normal, aa = :solid)
+      raise MiyakoError, "not have Drawing attribute! #{attribute}" unless @@draw_list[:ellipse].has_key?(attribute)
+      raise MiyakoError, "not have Drawing anti aliasing mode! #{aa}" unless @@draw_list[:ellipse][attribute].has_key?(aa)
+      @@draw_list[:ellipse][attribute][aa].call(sprite.bitmap, rect.to_a << Color.to_rgb(color))
     end
   end
 end
